1507ea6b55460d029678932299cc117c298af16c
StormSkoglund <125978969+StormSkoglund@users.noreply.github.com>
2023-06-08 00:56:45 +0200

    Add comments to HTML, CSS, javaScript

--- js/budgie.js
+++ js/budgie.js
@@ -53,17 +53,23 @@ var budgie = {
     listOfFacts.style.display = "block";
   },
 };
+/* The last element in the function above was added in order to show the object when the function gets called upon, the HTML element, called article, will otherwise stay hidden.
+in order to achieve this, i followed the short tutorial on this website https://www.shecodes.io/athena/3469-how-to-display-text-when-a-button-is-clicked-with-javascript [viewed on 07-06-2023], NB I found out that this site is driven by a chatbot of sorts,  I didn't know this initially, it seemed like a regular coding blog, I am unsure if this is acceptable. Full disclaimer, I didn't tell a bot or anything to generate code, I found this in a discussion thread. */
+
+// The variables for both buttons are defined underneath, they will make interaction with functions possible.
 
 var buttonBite = document.querySelector(".bite");
 var buttonFacts = document.querySelector(".fact-box");
-
+// Selecting the body and h2 element, they will be a part of one of the functions.
 var heading = document.querySelector(".pet");
 var background = document.querySelector("body");
+//Function whereas you get bitten by Ove the budgie, if you try to pet him (Background changes color to red and a warning text appears).
 
 function newHeadingAndBackground() {
   heading.innerHTML = "You've been bitten";
   heading.style.fontSize = "xxx-large";
   background.style.backgroundColor = "red";
 }
+//Calling both functions, the first function controls the bite warning and background color, the second makes the object appear as a list
 buttonBite.onclick = newHeadingAndBackground;
 buttonFacts.onclick = budgie.featherFacts;
